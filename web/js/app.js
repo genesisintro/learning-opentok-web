fc1fe11067bb48eb12d878126ef4d3c1fd050d81
genesisintro <anup88.2010@gmail.com>
2016-08-31 15:28:03 +0530

    update app.js

--- web/js/app.js
+++ web/js/app.js
@@ -1,30 +1,201 @@
-var apiKey,
-    sessionId,
-    token;
+/* global OT CommunicationAccPack */
+(function () {
 
-$(document).ready(function() {
-  // Make an Ajax request to get the OpenTok API key, session ID, and token from the server
-  $.get(SAMPLE_SERVER_BASE_URL + '/session', function(res) {
-    apiKey = res.apiKey;
-    sessionId = res.sessionId;
-    token = res.token;
+  // Modules
+  var _communication;
 
-    initializeSession();
-  });
-});
+  // OpenTok session
+  var _session;
 
-function initializeSession() {
-  var session = OT.initSession(apiKey, sessionId);
+  // Application State
+  var _initialized = false;
+  var _callActive = false;
+  var _remoteParticipant = false;
+  var _callProps = {
+    enableLocalAudio: true,
+    enableLocalVideo: true,
+    enableRemoteAudio: true,
+    enableRemoteVideo: true,
+  };
 
-  // Subscribe to a newly created stream
-  session.on('streamCreated', function(event) {
-    session.subscribe(event.stream, 'subscriber', {
+  // Options hash
+  var _options = {
+    apiKey: '45589022', // Replace with your OpenTok API key
+    sessionId: '2_MX40NTY0NDg1Mn5- MTQ3MjQ1MTMxOTA4Nn4yZDcvcUZ1cDZFOXBuWnpkWGZFbWx1T2Z -fg', // Replace with a generated Session ID
+    token: 'T1==cGFydG5lcl9pZD00NTY0NDg1MiZzZGtfdmVyc2lvbj1kZWJ1Z2dlciZzaWc9ZGE4YjI0ZGFhNmNmOGQwYmQzZjg1ZGNiMGU0ZmJlNTlmOTJjYTk5MTpzZXNzaW9uX2lkPTFfTVg0ME5UWTBORGcxTW41LU1UUTNNall5TlRnd01ESTFObjVEUmt4WlEybFJTM2RCVjNoTmNqTTBRbkYzVmtOWVFqQi1mZyZjcmVhdGVfdGltZT0xNDcyNjI1ODAwJnJvbGU9bW9kZXJhdG9yJm5vbmNlPTE0NzI2MjU4MDAuMzA0OTY3NjIyODQyNSZleHBpcmVfdGltZT0xNDc1MjE3ODAw', // Replace with a generated token
+    localCallProperties: {
       insertMode: 'append',
       width: '100%',
-      height: '100%'
+      height: '100%',
+      showControls: false,
+      style: {
+        buttonDisplayMode: 'off'
+      }
+    }
+  };
+
+  /** DOM Helper Methods */
+  var _makePrimaryVideo = function (element) {
+    $(element).addClass('primary-video');
+    $(element).removeClass('secondary-video');
+  };
+
+  var _makeSecondaryVideo = function (element) {
+    $(element).removeClass('primary-video');
+    $(element).addClass('secondary-video');
+  };
+
+  // Swap positions of the small and large video elements when participant joins or leaves call
+  var _swapVideoPositions = function (type) {
+
+    if (type === 'start' || type === 'joined') {
+
+      _makePrimaryVideo('#videoHolderBig');
+      _makeSecondaryVideo('#videoHolderSmall');
+
+      /**
+       * The other participant may or may not have joined the call at this point.
+       */
+      if (!!_remoteParticipant) {
+        $('#remoteControls').show();
+        $('#videoHolderBig').show();
+      }
+
+    } else if ((type === 'end' && !!_remoteParticipant) || type === 'left') {
+
+      _makePrimaryVideo('#videoHolderSmall');
+      _makeSecondaryVideo('#videoHolderBig');
+
+      $('#remoteControls').hide();
+      $('#videoHolderBig').hide();
+    }
+
+  };
+
+  // Toggle local or remote audio/video
+  var _toggleMediaProperties = function (type) {
+
+    _callProps[type] = !_callProps[type];
+
+    _communication[type](_callProps[type]);
+
+    $(['#', type].join('')).toggleClass('disabled');
+
+  };
+
+
+  var _startCall = function () {
+
+    // Start call
+    _communication.start();
+    _callActive = true;
+
+    // Update UI
+    $('#callActive').addClass('active');
+    $('#videoHolderSmall').addClass('active');
+
+    $('#enableLocalAudio').show();
+    $('#enableLocalVideo').show();
+
+    if (_remoteParticipant) {
+      _swapVideoPositions('start');
+    }
+
+  };
+
+  var _endCall = function () {
+
+    // End call
+    _communication.end();
+    _callActive = false;
+
+    // Update UI
+    $('#callActive').toggleClass('active');
+
+    $('#enableLocalAudio').hide();
+    $('#enableLocalVideo').hide();
+
+    if (_callActive || _remoteParticipant) {
+      _swapVideoPositions('end');
+    }
+  };
+
+  var _addEventListeners = function () {
+
+    // Call events
+    _session.on('streamCreated', function (event) {
+
+      if (event.stream.videoType === 'camera') {
+        _remoteParticipant = true;
+        if (_callActive) {
+          _swapVideoPositions('joined');
+        }
+      }
+
     });
+
+    _session.on('streamDestroyed', function (event) {
+
+      if (event.stream.videoType === 'camera') {
+        _remoteParticipant = false;
+        if (_callActive) {
+          _swapVideoPositions('left');
+        }
+      }
+    });
+
+    // Click events for enabling/disabling audio/video
+    var controls = [
+      'enableLocalAudio',
+      'enableLocalVideo',
+      'enableRemoteAudio',
+      'enableRemoteVideo'
+    ];
+    controls.forEach(function (control) {
+      $(['#', control].join('')).on('click', function () {
+        _toggleMediaProperties(control);
+      });
+    });
+  };
+
+  var _init = function () {
+
+    // Get session
+    _session = OT.initSession(_options.apiKey, _options.sessionId);
+
+    // Connect
+    _session.connect(_options.token, function (error) {
+      if (error) {
+        console.log('Session failed to connect');
+      } else {
+        _communication = new CommunicationAccPack(_.extend(_options, {
+          session: _session,
+          localCallProperties: _options.localCallProperties
+        }));
+        _addEventListeners();
+        _initialized = true;
+        _startCall();
+      }
+    });
+
+  };
+
+  var _connectCall = function () {
+
+    if (!_initialized) {
+      _init();
+    } else {
+      !_callActive ? _startCall() : _endCall();
+    }
+
+  };
+
+  // Start or end call
+  document.addEventListener('DOMContentLoaded', function () {
+    $('#callActive').on('click', _connectCall);
   });
 
+}());
   session.on('sessionDisconnected', function(event) {
     console.log('You were disconnected from the session.', event.reason);
   });
